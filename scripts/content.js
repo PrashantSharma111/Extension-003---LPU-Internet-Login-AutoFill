0e7b57aa303b8073a531009aea220150477fceb1
Yash Kumar Gupta <ykgupta2411@gmail.com>
2024-09-21 00:32:25 +0530

    Fixed filling of login details before button click

--- scripts/content.js
+++ scripts/content.js
@@ -4,7 +4,24 @@ const saveInfo = document.querySelector("input[name='saveinfo']");
 const agreePolicy = document.querySelector("#agreepolicy");
 const loginBtn = document.querySelector("#loginbtn");
 
+if(loginBtn)
+{
+    loginBtn.parentElement.innerHTML += `<button id='autoBtn' class='buttonstyle'>Auto</button>`;
+    const autoBtn = document.getElementById('autoBtn');
+    autoBtn.addEventListener('click', login);
+}
+
 const login = () => {
+
+    if(username)
+        username.value = "<Your Registration Number>";
+    if(password)
+        password.value = "<Your Internet Password>";
+    if(loginBtn)
+        loginBtn.disabled = false;
+    if(agreePolicy)
+        agreePolicy.checked = false;
+
     if(saveInfo)
         saveInfo.checked = true;
     if(agreePolicy)
@@ -13,19 +30,3 @@ const login = () => {
         loginBtn.click();
     console.log("Logged in");
 }
-
-if(loginBtn)
-{
-    loginBtn.parentElement.innerHTML += `<button id='autoBtn' class='buttonstyle'>Auto</button>`;
-    const autoBtn = document.getElementById('autoBtn');
-    autoBtn.addEventListener('click', login);
-}
-
-if(username)
-    username.value = "<Your Registration Number>";
-if(password)
-    password.value = "<Your Internet Password>";
-if(loginBtn)
-    loginBtn.disabled = false;
-if(agreePolicy)
-    agreePolicy.checked = false;
